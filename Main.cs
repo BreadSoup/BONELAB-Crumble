af1bbc741359f5586359be6dfec7d3ea7dae2b1d
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-07-17 16:26:11 -0400

    Base is finished

--- Main.cs
+++ Main.cs
@@ -1,17 +1,35 @@
 ﻿using MelonLoader;
+using SLZ.VRMK;
+using BoneLib;
+using UnityEngine;
 
 namespace bonelab_template
 {
     internal partial class Main : MelonMod
     {
+        bool SceneLoaded = false;
+        PhysGrounder feet = null;
+        bool PreviousFrame;
+        bool CurrentFrame;
+        float Threshold = 5;
+        float Markiplier = 1.3f; //https://shorturl.at/opMO3
+
         public override void OnEarlyInitializeMelon()
         {
             base.OnEarlyInitializeMelon();
         }
 
+
         public override void OnInitializeMelon()
         {
-            base.OnInitializeMelon();
+            BoneLib.Hooking.OnLevelInitialized += (_) => { OnSceneAwake(); };
+        }
+
+        public void OnSceneAwake()
+        { 
+            SceneLoaded = true;
+            feet = BoneLib.Player.physicsRig.feet.GetComponent<PhysGrounder>();
+            PreviousFrame = feet.isGrounded;
         }
 
         public override void OnLateInitializeMelon()
@@ -21,7 +39,23 @@ namespace bonelab_template
 
         public override void OnUpdate()
         {
-            base.OnUpdate();
+            if (SceneLoaded)
+            {
+
+                CurrentFrame = feet.isGrounded;
+
+                if (CurrentFrame && !PreviousFrame)
+                {
+                    if (BoneLib.Player.physicsRig.wholeBodyVelocity.y < -Threshold)
+                    {
+                        float damage = Mathf.Abs(BoneLib.Player.physicsRig.wholeBodyVelocity.y + Threshold);
+                        damage *= Markiplier; // I might be able to put this on the line above but I don't know the order of operations and I dont care enough to find out because this works
+                        Player.rigManager.health.TAKEDAMAGE(damage);
+                    }
+                }
+
+                PreviousFrame = CurrentFrame;
+            }
         }
 
         public override void OnFixedUpdate()
