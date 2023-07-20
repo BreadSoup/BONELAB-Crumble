e3a55e5869f50ddafab6e0b193a613d9218b8bd3
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-07-19 23:56:47 -0400

    I forgot to save

--- Preferences.cs
+++ Preferences.cs
@@ -11,19 +11,34 @@ namespace bonelab_template
 {
     internal class Preferences
     {
+
+        public static MelonPreferences_Category MelonPrefCategory { get; private set; }
+        public static MelonPreferences_Entry<bool> MelonPrefEnabled { get; private set; }
+
+        public static void MelonPreferencesCreator
+        {
+            MelonPrefCategory = MelonPreferences.CreateCategory("Crumble");
+            MelonPrefEnabled = MelonPrefCategory.CreateEntry<bool>("IsEnabled", true, null, null, false, false, null, null);
+            IsEnabled = MelonPrefEnabled.Value;
+        }
+
         public TMPro.TMP_Text textMesh;
         public static void BonemenuCreator()
         {
             var category = MenuManager.CreateCategory(
-                "<color=#FF0000>C</color>" +
-                "<color=#FF2900>r</color>" +
-                "<color=#FF5200>u</color>" +
-                "<color=#FF7B00>m</color>" +
-                "<color=#FFA400>b</color>" +
-                "<color=#FFCD00>l</color>" +
-                "<color=#FFFA00>e</color>"
+                "<color=#ff2424>C</color>" +
+                "<color=#ff2926>r</color>" +
+                "<color=#ff2e28>u</color>" +
+                "<color=#ff332a>m</color>" +
+                "<color=#ff372c>b</color>" +
+                "<color=#ff3a2e>l</color>" +
+                "<color=#ff3e30>e</color>"
                 , Color.white);
-
+            category.CreateBoolElement("Mod Toggle", Color.yellow, IsEnabled, new Action<bool>(OnSetEnabled));
+            var colorR = category.CreateFloatElement("Damage Threshold", Color.red, Main.Threshold, 1f, 0f, 100f, (dt) =>
+            {
+                Main.Threshold = dt;
+            });
         }
     }
 
